3327dc5dca00a89c3cdb263bdfa8bb9a70fd925a
RogerProgrammes <66995721+RogerProgrammes@users.noreply.github.com>
2020-06-16 10:59:26 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+// This program converts celcius to fahrenheit 
+
+function celciusToFahrenheit (n){
+  var celcius = (n*9/5) + 32;
+  return celcius;
+}
+var x = 5;
+console.log (celciusToFahrenheit(x));
